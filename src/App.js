1c8a3a66b635820528d2313fad00bca621e77e14
Brodie Brock <brodiebrock@gmail.com>
2022-09-28 17:55:58 -0500

    Navigate is not being used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
