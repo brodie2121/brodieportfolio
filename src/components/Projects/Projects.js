3be1f67f9e0d1481aa0a00ee13675afe82eded8e
Brodie Brock <brodiebrock@gmail.com>
2023-10-06 15:46:46 -0500

    fixing text alignment in projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,7 +14,7 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="green">Works </strong>
         </h1>
-        <p style={{ color: "white" }}>
+        <p style={{ color: "white", textAlign: "center" }}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
